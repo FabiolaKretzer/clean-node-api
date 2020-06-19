450a4ea2772cb150e77bca74dc41629b8025e1f3
Fabiola Kretzer <fabiolakretzer@gmail.com>
2020-06-19 18:34:13 -0300

    ensure AuthUseCase returns null if an invalid password is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,13 +1,15 @@
 const AuthUseCase = require('./auth-usecase')
-const { MissingParamError, InvalidParamError } = require('../../utils/errors')
+const { MissingParamError } = require('../../utils/errors')
 
 const makeSut = () => {
   class LoadUserByEmailRepositorySpy {
     async load (email) {
       this.email = email
+      return this.user
     }
   }
   const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
+  loadUserByEmailRepositorySpy.user = {}
   const sut = new AuthUseCase(loadUserByEmailRepositorySpy)
   return {
     sut,
@@ -37,18 +39,25 @@ describe('Auth UseCase', () => {
   test('Should throw if no LoadUserByEmailRepository is provided', async () => {
     const sut = new AuthUseCase()
     const promisse = sut.auth('any_email@gmail.com', 'any_password')
-    expect(promisse).rejects.toThrow(new MissingParamError('loadUserByEmailRepository'))
+    expect(promisse).rejects.toThrow()
   })
 
   test('Should throw if LoadUserByEmailRepository has no load method', async () => {
     const sut = new AuthUseCase({})
     const promisse = sut.auth('any_email@gmail.com', 'any_password')
-    expect(promisse).rejects.toThrow(new InvalidParamError('loadUserByEmailRepository'))
+    expect(promisse).rejects.toThrow()
   })
 
-  test('Should return null if LoadUserByEmailRepository returns null', async () => {
-    const { sut } = makeSut()
+  test('Should return null if an invalid email is provided', async () => {
+    const { sut, loadUserByEmailRepositorySpy } = makeSut()
+    loadUserByEmailRepositorySpy.user = null
     const accessToken = await sut.auth('invalid_email@gmail.com', 'any_password')
+    expect(accessToken).toBeNull()
+  })
+
+  test('Should return null if an invalid password is provided', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.auth('any_email@gmail.com', 'invalid_password')
     expect(accessToken).toBeNull()
   })
 })

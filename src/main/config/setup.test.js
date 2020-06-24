77199eedf4c2f614f832694b157e6cb5683a26cb
Fabiola Kretzer <fabiolakretzer@gmail.com>
2020-06-24 17:14:06 -0300

    enable CORS

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -7,7 +7,18 @@ describe('App Setup', () => {
       res.send('')
     })
 
-    const res = await request(app).get('/')
+    const res = await request(app).get('/test_x_powered_by')
     expect(res.headers['x-powered-by']).toBeUndefined()
+  })
+
+  test('Should enable cors', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send('')
+    })
+
+    const res = await request(app).get('/test_cors')
+    expect(res.headers['access-controll-allow-origin']).toBe('*')
+    expect(res.headers['access-controll-allow-methods']).toBe('*')
+    expect(res.headers['access-controll-allow-headers']).toBe('*')
   })
 })

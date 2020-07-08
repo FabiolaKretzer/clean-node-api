664a5b93807fd9a7705875e64ba21f95aae79dd3
Fabiola Kretzer <fabiolakretzer@gmail.com>
2020-07-08 09:26:45 -0300

    remove dbName from MongoHelper

--- src/main/adapters/express-router-adapter.js
+++ src/main/adapters/express-router-adapter.js
@@ -1,8 +1,8 @@
 module.exports = class ExpressRouterAdapter {
   static adapt (router) {
-    return async(req, res) => {
-      const httpRequest ={
-        body:req.body
+    return async (req, res) => {
+      const httpRequest = {
+        body: req.body
       }
       const httpResponse = await router.route(httpRequest)
       res.status(httpResponse.statusCode).json(httpResponse.body)
